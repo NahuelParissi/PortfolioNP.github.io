921157dc4c730a98fff6b682bf14f01757416b10
NahuelP <nahuelparissi@gmail.com>
2024-07-25 18:51:15 -0300

    Add emailJS

--- src/sections/contact/contactS.jsx
+++ src/sections/contact/contactS.jsx
@@ -1,27 +1,67 @@
 import { useForm } from 'react-hook-form';
-import './contactS.css'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import './contactS.css';
 
-export default function ContactS() {
+const ContactS = () => {
 
+  const form = useRef();
   const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
-  console.log(errors);
-  
+
+  const sendEmail = (formData) => {
+    emailjs
+      .sendForm('service_969uy9e', 'template_0109oay', form.current, 'IF5STj-_nNhzmf1MB')
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          alert('Message sent successfully!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+          alert('Failed to send the message, please try again.');
+        }
+      );
+  };
+
   return (
 
-    <form onSubmit={handleSubmit(onSubmit)} className='bodyContactS'>
+    <form className='bodyContactS' ref={form} onSubmit={handleSubmit(sendEmail)}>
 
       <h3 className='formHeader'>Talk with me BRO 🐱‍👤</h3>
 
-      <input className='formName' type="text" placeholder="Nombre" {...register("Name", {})} />
+      <div className='formName'>
 
-      <input className='formEmail' type="email" placeholder="Email" {...register("Email", {pattern: /@/i})} />
+        <label className='formElement'>Nombre</label>
+        <input className='formElement' type="text" placeholder='Nombre'{...register('user_name', { required: true })} />
+        {errors.user_name && <span>This field is required </span>}
 
-      
-      <textarea className='formTextArea' placeholder="Tu mensaje" {...register} />
+      </div>
 
-      <input className='formButton' type="submit" />
+      <div className='formEmail'>
 
+        <label className='formElement'>Email</label>
+        <input className='formElement' type="email" placeholder='Email' {...register('user_email', {
+          required: true, pattern: {
+            value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+            message: 'Invalid email address'
+          }
+        })} />
+
+        {errors.user_email && <span>{errors.user_email.message}</span>}
+
+      </div>
+
+      <div className='formTextArea'>
+        <label className='formElement'>Tu mensaje</label>
+        <textarea placeholder='Dejame tu mensaje aqui' {...register('message', { required: true })} />
+        {errors.message && <span>This field is required</span>}
+      </div>
+
+      <input className='formButton' type="submit" value="Send" />
     </form>
   );
 }
+
+export default ContactS;
+
+// Cambiar "<span>This field is required</span>" de los campos por otros mas aestetick
